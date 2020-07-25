8fa3b5768530286a574c5c7fb8e3029c63075da8
Loooffy <Loooffy@gmail.com>
2020-07-26 02:14:58 +0800

    add topic API

--- server/controller/root_controller.js
+++ server/controller/root_controller.js
@@ -3,13 +3,13 @@ const Quiz = require('../model/quiz_model');
 const QA = require('../model/QA_model');
 
 const renderQuizPage = async (req, res) => {
-    let quiz_id = req.query.quiz_id
+    let qid = req.query.qid
     //let result = await Quiz.getQuizData()
-    let result = await QA.getQAData(quiz_id)
+    let result = await QA.getQAData(qid)
     //result.data.options = JSON.parse(result.data.options)
     //result.data.question = marked(result.data.question)
     //res.render('index', result.data)
-    res.render('quiz', {quiz_id: quiz_id})
+    res.render('quiz', {qid: qid})
 }
     
 module.exports = {
