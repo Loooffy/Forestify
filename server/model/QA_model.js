d8deabb00250d7eb910e22ebdb67f85b31c5f24b
Loooffy <Loooffy@gmail.com>
2020-07-27 00:16:44 +0800

    apply pretty format on sql query string

--- server/model/QA_model.js
+++ server/model/QA_model.js
@@ -1,8 +1,31 @@
 const { query, transaction, commit, rollback } = require('../../util/mysqlCon.js');
 
-const getQAData = async (quiz_id) => {
-    console.log(quiz_id)
-    const results = await query('SELECT QA.*, student.id as owner_id, student.name as owner_name, v.total_vote from QA inner join quiz on quiz.id = QA.quiz_id inner join student as student on QA.owner_id = student.id inner join (select QA_id, sum(vote) as total_vote from votes group by QA_id) as v on QA.id = v.QA_id where quiz.id = ? order by QA.post_time DESC', [quiz_id])
+const getQAData = async (qid) => {
+    let QAQ = 
+        `
+            SELECT 
+                QA.*,
+                student.id AS owner_id,
+                student.name AS owner_name,
+                v.total_vote
+            FROM
+                QA
+                    INNER JOIN
+                quiz ON quiz.id = QA.quiz_id
+                    INNER JOIN
+                student AS student ON QA.owner_id = student.id
+                    INNER JOIN
+                (SELECT 
+                    QA_id, SUM(vote) AS total_vote
+                FROM
+                    votes
+                GROUP BY QA_id) AS v ON QA.id = v.QA_id
+            WHERE
+                quiz.id = ?
+            ORDER BY QA.post_time DESC
+        `
+
+    const results = await query(QAQ, [qid])
     return {data: results}
     }
 
