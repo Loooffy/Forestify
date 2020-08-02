a9879bc14113c041e1c125c585eca6da12c98e7d
Loooffy <Loooffy@gmail.com>
2020-08-02 22:38:11 +0800

    add map router

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use('/api/' ,
         require('./server/route/vote_route'),
         require('./server/route/user_route'),
         require('./server/route/topic_route'),
+        require('./server/route/map_route'),
     ]
 );
 
